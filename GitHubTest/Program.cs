79d46da0e24acbe320c021976cc2bf89cb6fa8e0
Jonas Lærke Hellum <jonas_laerke@hotmail.com>
2024-01-21 23:51:18 +0100

    Added a new WriteLine and a method

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Jonas L. Hellum");
+        CountTo(10);
+    }
+
+    static void CountTo(int num)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
